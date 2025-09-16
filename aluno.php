27eb7eea69e9edd848837a681c080cfa87e0194c
Luís Arthur Turck da Silva <luisarthur0207@gmail.com>
2025-09-16 13:42:09 -0400

    Add files via upload

--- aluno.php
+++ aluno.php
@@ -46,6 +46,44 @@ $luis-> adicionarnota(10,);
 $luis-> media();
 $luis->aprovado();
 
+class Turma{
+    public $diciplina;
+    public $alunos=[];
+    public $media;
+    public $melhor;
+    public $ap;
+    public $al;
+
+    public function __construct($diciplina) {
+        $this->diciplina = $diciplina;
+    }
+
+    public function adicionaraluno(Aluno $aluno ){
+            array_push($this->alunos, $aluno);
+    }
+    public function melhoraluno() {
+         $media=0;
+         for ($x=0; $x<sizeof($this->alunos); $x++) { 
+            
+            if ($this->alunos[$x]->media()>$media){
+                $media=$this->alunos[$x]->media();
+                $melhor=$this->alunos[$x];
+            }
+         }
+          echo"o melhor aluno foi $melhor com nota de $media";  
+    }
+    public function resultadofinal(){
+        for ($x=0; $x<sizeof($this->alunos); $x++) { 
+            $ap=$this->alunos[$x]->aprovado();
+            $al=$this->alunos[$x];
+            echo"o aluno $al foi $ap";
+         }
+    }
+}
+$aula=new Turma("mat");
+$aula->adicionaraluno($luis);
+$aula->melhoraluno();
+$aula->resultadofinal();
 
 
 
